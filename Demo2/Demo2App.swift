cabe9d6da67cf29510c3c30ac4d7197a570c365c
Dirk Hostens <dirk.hostens@vives.be>
2025-10-21 08:48:30 +0200

    Initial Commit

---
+++ Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by Dirk Hostens on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
